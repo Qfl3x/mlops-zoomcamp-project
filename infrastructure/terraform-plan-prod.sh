553b1f76aa733ff6067d1ecc1edb102c5133d72a
Qfl3x <qfl3x@protonmail.com>
2022-08-22 15:51:44 +0000

    Working

--- infrastructure/terraform-plan-prod.sh
+++ infrastructure/terraform-plan-prod.sh
@@ -1,3 +1,3 @@
 export $(cat .env)
 export GOOGLE_APPLICATION_CREDENTIALS=./terraform-account.json
-terraform plan -var="gcp_project_id="$PROJECT_ID"" -var="service-account="$GOOGLE_ACCOUNT_NAME"" --var-file vars/prod.tfvars
+terraform plan -var="gcp_project_id="$PROJECT_ID"" -var="service-account="$GOOGLE_ACCOUNT_NAME"" --var-file vars/prod.tfvars -lock=false
